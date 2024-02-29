cd187d168d6f2ca85d54f49b6a248aa3cbda0934
TomTD <nes211nes211nes@gmail.com>
2024-02-29 07:17:05 +0100

    Initial commit

---
+++ src/main/java/pl/tdelektro/carhelper/CarhelperApplication.java
@@ -0,0 +1,13 @@
+package pl.tdelektro.carhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tdelektro/carhelper/CarhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tdelektro.carhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
