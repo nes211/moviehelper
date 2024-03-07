b5e43d4d67ba349277b730f3022a9426933ae926
TomTD <nes211nes211nes@gmail.com>
2024-03-07 07:49:51 +0100

    Additional files

--- src/main/java/pl/tdelektro/moviehelper/configuration/CorsFilter.java
+++ src/main/java/pl/tdelektro/moviehelper/configuration/CorsFilter.java
@@ -1,6 +1,5 @@
 package pl.tdelektro.moviehelper.configuration;
 
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -11,18 +10,14 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        //response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        //response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
         chain.doFilter(req, res);
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        // Initialization code, if any
     }
 
     @Override
     public void destroy() {
-        // Cleanup code, if any
     }
 }
